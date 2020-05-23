7d3dab13d4d13b45193d1fcaa2089eb2c6b35964
Jared Rothenberg <jaredar@gmail.com>
2020-05-23 06:23:40 -0700

    fix cors

--- index.js
+++ index.js
@@ -15,20 +15,9 @@ app.use(
     origin: process.env.FONT_END_URL,
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
+    "Acces-Control-Allow-Origin": "*",
   })
 );
-app.options("*", cors());
-
-app.all("", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jaredar.com");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  //Auth Each API Request created by user.
-  next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -46,7 +35,7 @@ app.post("/", async (req, res) => {
     to: "jaredar@gmail.com",
     from: `${email}`,
     subject: `${subject}`,
-    text: `${message}`,
+    text: `Name: ${name}. Message: ${message}`,
     html: `<strong>${message}</strong>`,
   };
 
